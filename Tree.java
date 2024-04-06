ccc73a0165823430de0c76d1c7873587a7958652
lktika <lktika24@gmail.com>
2024-04-06 22:13:11 +0800

    test

---
+++ Tree.java
@@ -0,0 +1,31 @@
+// Define your tree class in this file!
+public class Tree {
+  public static void main(String[] args) {
+    //Introducing myself and printing a tree to the screen
+    
+    System.out.println("Hey there, I'm Ariel and I'm learning to code in Java!");
+    System.out.println("Planting a Tree");
+    System.out.println("I'm going to plant a tree today!");
+    System.out.println("Ready to get my hands dirty!");
+    
+    System.out.println("  *  ");
+    System.out.println(" *** ");
+    System.out.println(" *** ");
+    System.out.println("  *  ");
+    System.out.println("  *  ");
+
+    System.out.println(" * ** *");
+    System.out.println(" ******");
+    System.out.println("  **** ");
+    System.out.println("   **  ");
+    System.out.println("~~~~~~~~~~");
+
+    System.out.println(" ***** ");
+    System.out.println(" ***** ");
+    System.out.println(" ***** ");
+    System.out.println("   *   ");
+    System.out.println("   *   ");
+    System.out.println("#######");
+    
+  }
+}
