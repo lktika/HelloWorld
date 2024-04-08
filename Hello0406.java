4ae88f00c60e3aa3ee55a56262dd501bac19e965
lktika <lktika24@gmail.com>
2024-04-08 22:03:43 +0800

    test

---
+++ Hello0406.java
@@ -0,0 +1,22 @@
+public class Hello0406 {
+
+	//编写一个main方法
+	public static void main(String[] args) {
+		System.out.println("hello,world~");
+	}
+}
+
+class Tiger {
+
+	public static void main(String[] args) {
+		System.out.println("hello, 小老虎~");
+	}
+	
+}
+
+class Cat {
+
+    public static void main(String[] args) {
+    	System.out.println("hello, 小猫~");
+    }
+}
---
+++ MyProfile.java
@@ -0,0 +1,9 @@
+public class MyProfile {
+	public static void main(String[] args) {
+    String name = "Raka";
+    int age = 29;
+    double desiredSalary = 120000.00;
+    char gender = 'm';
+    boolean lookingForJob = true;
+	}
+}
