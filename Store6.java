7de4c7f52b115da24f41418fec156f5996573c07
lktika <lktika24@gmail.com>
2024-04-16 22:10:37 +0800

    test

---
+++ Store6.java
@@ -0,0 +1,19 @@
+public class Store6 {
+  // instance fields
+  String productType;
+  
+  // constructor method
+  public Store6(String product) {
+    productType = product;
+  }
+  
+  // advertise method
+  public void advertise() {
+     System.out.println("Come spend some money!");
+     System.out.println("Selling " + productType + "!");
+  }
+  // main method
+  public static void main(String[] args) {
+    
+  }
+}
