7351eb3645df4cbec0dcc7af9420873f0e9e84b1
lktika <lktika24@gmail.com>
2024-04-22 22:19:58 +0800

    test

---
+++ ArithmeticOperator.java
@@ -0,0 +1,16 @@
+/**
+ * 演示算数运算符的使用
+ */
+public class ArithmeticOperator {
+	
+	//编写一个main方法
+	public static void main(String[] args) {
+		// /使用
+		System.out.println(10 / 4); //从数学来看是2.5，java中 2
+		System.out.println(10.0 / 4); //jjava是2.5
+		// 注释快捷键 ctrl + /,再次输入 ctrl + / 取消注释
+		double d = 10 / 4;//java中10 / 4 = 2, 2=>2.0
+		System.out.println(d);// 是2.0
+		
+	}
+}
