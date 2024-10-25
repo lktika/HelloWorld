8e804ba4ff419efd790303b0afdcac8695ed96b0
LAPTOP-VGDQQ6UA\lktika <lktika24@gmail.com>
2024-10-25 10:57:51 +0900

    test

---
+++ SinoTibetan.java
@@ -0,0 +1,12 @@
+class SinoTibetan extends Language {
+  
+  //SinoTibetan(String languageName, int speakers) {
+    //super(languageName, speakers, "Asia", "subject-object-verb");
+
+  SinoTibetan(String languageName, int speakers) {
+    super(languageName, speakers, "Asia", "subject-object-verb");
+    if (languageName.contains("Chinese")) {
+      this.wordOrder = "subject-verb-object";
+    }
+  }
+}
