f2edbfddb5f91b1cbd132c74dd66334dacf344de
LAPTOP-VGDQQ6UA\lktika <lktika24@gmail.com>
2024-11-25 09:34:51 +0900

    test

---
+++ Main12.java
@@ -0,0 +1,18 @@
+public class Main12 {
+	public static void main(String[] args) {
+		// Declare a 2d array of float values called floatTwoD  
+    float[][] floatTwoD;   
+    floatTwoD = new float[4][10];  
+    char[][] ticTacToe = {
+      {'X', 'O', 'O'},
+      {'O', 'X', ' '},
+      {'X', ' ', 'X'}
+    };
+
+    ticTacToe = new char[][]{
+      {'O', 'X', 'X'},
+      {'X', 'O', ' '},
+      {'O', ' ', 'O'}
+    };
+    }
+}
---
+++ Main13.java
@@ -0,0 +1,12 @@
+public class Main13 {
+	public static void main(String[] args) {
+		// Using the provided 2D array
+	  int[][] intMatrix = {
+				{1, 1, 1, 1, 1},
+				{2, 4, 6, 8, 0},
+				{9, 8, 7, 6, 5}
+		};
+    int retrievedInt = intMatrix[0][3];
+    System.out.println(intMatrix[1][2] * 3);
+  }
+}
